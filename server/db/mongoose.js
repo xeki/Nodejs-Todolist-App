0590b160d45b4e7947f1f12b5b8564aff2df6e7a
Zer Men <zekey_05@yahoo.com>
2017-10-05 17:02:39 +0300

    dbpath modified

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.Promise = bluebird;
 var options={promiseLibrary:bluebird,useMongoClient:true};
 //mongoose.connect("mongodb://localhost:27017/TodoApp",options);
 
-mongoose.connect("mongodb://developer:Dev123Pass@ds111885.mlab.com:11885/todoapp",options);
+mongoose.connect("mongodb://dev:Dev123Pass@ds111885.mlab.com:11885/todoapp",options);
 
 
 module.exports ={mongoose};
