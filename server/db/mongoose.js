9d0162f9c4b0989e6c5b4d65896aca2c777511cb
Zer Men <zekey_05@yahoo.com>
2017-10-05 16:45:17 +0300

    deploying to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,9 @@ const bluebird = require("bluebird");
 //mongoose.Promise = global.Promise;
 mongoose.Promise = bluebird;
 var options={promiseLibrary:bluebird,useMongoClient:true};
-mongoose.connect("mongodb://localhost:27017/TodoApp",options);
+//mongoose.connect("mongodb://localhost:27017/TodoApp",options);
+
+mongoose.connect("mongodb://developer:<Dev123Pass>@ds111885.mlab.com:11885/todoapp",options);
+
 
 module.exports ={mongoose};
