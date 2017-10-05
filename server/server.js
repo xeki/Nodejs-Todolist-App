accac1e64a0763d73047ca5a90df4964e8e03dbf
Zer Men <zekey_05@yahoo.com>
2017-10-05 12:32:11 +0300

    GET /todos:id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const{ObjectId} = require("mongodb");
+
 
 var{mongoose}=require("./db/mongoose");
 var{Todo}=require("./models/todo");
@@ -21,6 +23,26 @@ app.get("/todos",(req,res)=>{
 
 });
 
+
+app.get("/todos/:id",(req,res)=>{
+
+console.log("GET Todos/:id request ",new Date());
+
+  let id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    res.status(404).send({error:"Invalid Id"});
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e)=>{
+    res.status(400).send({});
+  });
+
+});
 
 
 app.post("/todos",(req,res)=>{
